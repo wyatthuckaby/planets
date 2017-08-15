abac37ce0ea1ceac1fc540f69acd1958b70c59d8
Wyatt Huckaby <huckabywyatt@gmail.com>
2017-08-15 10:44:28 -0600

    Added new stuff

--- models/moon.js
+++ models/moon.js
@@ -1,8 +1,14 @@
 var mongoose = require("mongoose");
-
+var ObjectId = mongoose.Schema.ObjectId;
 
 var moonSchema = new mongoose.Schema({
-  name: {type: String, required:true}
+    name: { type: String, required: true },
+
+    starId: { type: ObjectId, ref: 'Star', required: true },
+    galaxyId: { type: ObjectId, ref: 'Galaxy', required: true },
+    planetId: { type: ObjectId, ref: 'Planet', required: true }
 })
 
 var Moon = mongoose.model("Moon", moonSchema);
+
+module.exports = Moon;
--- models/planet.js
+++ models/planet.js
@@ -2,12 +2,15 @@ var mongoose = require("mongoose");
 var ObjectId = mongoose.Schema.ObjectId
 
 var planetSchema = new mongoose.Schema({
-  name: {type: String, required:true},
+    name: { type: String, required: true },
 
-  // RELATIONSHIPS
-  starId: {type: ObjectId, ref: 'Star', required: true},
-  galaxyId: {type: ObjectId, ref: 'Galaxy', required: true}
+    // RELATIONSHIPS
+    starId: { type: ObjectId, ref: 'Star', required: true },
+    galaxyId: { type: ObjectId, ref: 'Galaxy', required: true }
 })
 
 
 var Planet = mongoose.model("Planet", planetSchema);
+
+
+module.exports = Planet;
